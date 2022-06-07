c8212a279bdcbc5fc827f5aabbb2a1aadc13c368
ansuj9123 <97033373+ansuj9123@users.noreply.github.com>
2022-06-07 10:19:55 +0100

    Add files via upload
    Mynew delivery site

---
+++ src/script.js
@@ -0,0 +1,12 @@
+let slider = tns({
+    container : ".my-slider",
+    "slideBy" : 1,
+    "speed" : 400,
+    "nav" : false,
+    controlsContainer: "#controls",
+    prevButton : ".previous",
+    nextButton : ".next",
+    
+     
+       
+})
